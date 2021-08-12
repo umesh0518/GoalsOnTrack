c29931c0c8dc38c8d85ee472aa367f82beba8a7a
Umesh Yadav <41235691+umesh0518@users.noreply.github.com>
2021-08-12 18:37:47 +0530

    First commit. Created 4 packages for each individual members. and also made homepage for all individual sections

---
+++ app/src/main/java/com/goalsontrack/flashcard/flashcard_home.java
@@ -0,0 +1,16 @@
+package com.goalsontrack.flashcard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.goalsontrack.R;
+
+public class flashcard_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashcard_home1);
+    }
+}
---
+++ app/src/main/java/com/goalsontrack/moneytracking/moneytracking_home.java
@@ -0,0 +1,16 @@
+package com.goalsontrack.moneytracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.goalsontrack.R;
+
+public class moneytracking_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moneytracking_home);
+    }
+}
