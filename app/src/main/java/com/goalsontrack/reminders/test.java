48031ba794e4b74d80bf649722793e1eb87a4c06
Umesh Yadav <41235691+umesh0518@users.noreply.github.com>
2021-08-13 14:49:11 +0530

    it was for testing

---
+++ app/src/main/java/com/goalsontrack/reminders/test.java
@@ -0,0 +1,16 @@
+package com.goalsontrack.reminders;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.goalsontrack.R;
+
+public class test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
