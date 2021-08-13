4609fdccb63103646141a7ecaa30c29b90f0f192
Umesh Yadav <41235691+umesh0518@users.noreply.github.com>
2021-08-13 15:54:45 +0530

    Everyone can clone from here now!

--- app/src/main/java/com/goalsontrack/reminders/reminder_homepage.java
+++ app/src/main/java/com/goalsontrack/reminders/reminder_homepage.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.goalsontrack.R;
 
-public class reminder_home extends AppCompatActivity {
+public class reminder_homepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_todo_home);
+        setContentView(R.layout.activity_reminder_homepage);
     }
 }
