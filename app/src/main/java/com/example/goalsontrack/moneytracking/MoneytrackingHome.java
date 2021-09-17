bd6d80c1281a2d33a83b30eae23cf3d458198523
Umesh Yadav <41235691+umesh0518@users.noreply.github.com>
2021-09-18 00:56:08 +0530

    folder structure is created to follow

---
+++ app/src/main/java/com/example/goalsontrack/moneytracking/MoneytrackingHome.java
@@ -0,0 +1,16 @@
+package com.example.goalsontrack.moneytracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.goalsontrack.R;
+
+public class MoneytrackingHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moneytracking_home);
+    }
+}
---
+++ app/src/main/java/com/example/goalsontrack/todo/TodoHome.java
@@ -0,0 +1,16 @@
+package com.example.goalsontrack.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.goalsontrack.R;
+
+public class TodoHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_home);
+    }
+}
