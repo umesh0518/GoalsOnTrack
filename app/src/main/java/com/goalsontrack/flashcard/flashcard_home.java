f26696fff077490322ae6b2a6c5de5742777ca15
Umesh Yadav <41235691+umesh0518@users.noreply.github.com>
2021-09-18 00:11:05 +0530

    started flashcard part

--- app/src/main/java/com/goalsontrack/flashcard/flashcard_home.java
+++ app/src/main/java/com/goalsontrack/flashcard/flashcard_home.java
@@ -3,6 +3,8 @@ package com.goalsontrack.flashcard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.goalsontrack.R;
 
@@ -12,5 +14,14 @@ public class flashcard_home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashcard_home);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.all_category_flashcard, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
