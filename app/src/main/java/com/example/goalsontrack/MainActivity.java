7a767a71e0cd462ccf67876b9142883878042bf3
Umesh Yadav <41235691+umesh0518@users.noreply.github.com>
2021-09-18 12:09:07 +0530

    flashcard homepage and homepage is linked

--- app/src/main/java/com/example/goalsontrack/MainActivity.java
+++ app/src/main/java/com/example/goalsontrack/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.goalsontrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.goalsontrack.flashcard.FlashcardHome;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // to start Flashcard subsystem
+    public void loadFlashcard(View view){
+        Intent startFlashcard = new Intent(this, FlashcardHome.class);
+
+        startActivity(startFlashcard);
+
     }
 }
