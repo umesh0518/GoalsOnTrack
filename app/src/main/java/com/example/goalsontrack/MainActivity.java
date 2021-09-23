f292231fdb11357ff26331cd14c8c9c0ba7ae379
arooshaRasheed <86253851+arooshaRasheed@users.noreply.github.com>
2021-09-23 20:20:29 +0430

    home page for now

--- app/src/main/java/com/example/goalsontrack/MainActivity.java
+++ app/src/main/java/com/example/goalsontrack/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.goalsontrack.flashcard.FlashcardHome;
+import com.example.goalsontrack.moneytracking.MoneytrackingHome;
+import com.example.goalsontrack.reminder.ReminderHome;
+import com.example.goalsontrack.todo.TodoHome;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +25,27 @@ public class MainActivity extends AppCompatActivity {
         Intent startFlashcard = new Intent(this, FlashcardHome.class);
         Toast.makeText(this,"Opening Flashcard", Toast.LENGTH_SHORT).show();
         startActivity(startFlashcard);
+
+    }
+
+    public void loadTodoList(View view){
+        Intent startTodoList = new Intent(this, TodoHome.class);
+        Toast.makeText(this,"Opening Todo List", Toast.LENGTH_SHORT).show();
+        startActivity(startTodoList);
+
+    }
+
+    public void loadMoneyTracking(View view){
+        Intent startMoneyTracking = new Intent(this, MoneytrackingHome.class);
+        Toast.makeText(this,"Opening Money Tracking", Toast.LENGTH_SHORT).show();
+        startActivity(startMoneyTracking);
+
+    }
+
+    public void loadReminder(View view){
+        Intent startReminder = new Intent(this, ReminderHome.class);
+        Toast.makeText(this,"Opening Reminders", Toast.LENGTH_SHORT).show();
+        startActivity(startReminder);
 
     }
 }
